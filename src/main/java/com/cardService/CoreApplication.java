27a2a5fcb67571ebc8d3a6f207f28aa411112f36
Vasil <vaskovasilev94@yahoo.com>
2020-06-02 16:57:20 +0300

    Fixed bad version of spring-boot autoconfigure.

--- src/main/java/com/cardService/CoreApplication.java
+++ src/main/java/com/cardService/CoreApplication.java
@@ -1,6 +1,8 @@
 package com.cardService;
 
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -12,8 +14,10 @@ import java.io.File;
 
 @SpringBootApplication
 public class CoreApplication implements DisposableBean, CommandLineRunner {
+    Logger logger = LoggerFactory.getLogger(CoreApplication.class);
 
     public static void main(String[] args) {
+
         SpringApplication.run(CoreApplication.class, args);
     }
 
